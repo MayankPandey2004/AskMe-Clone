41e406dc89645889e7a995bad71eb8af37cd041e
MayankPandey2004 <mpandey2004@gmail.com>
2024-08-20 02:45:59 +0530

    updated logout

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -59,10 +59,11 @@ const ProfileImage = styled.div`
   }
 `;
 
-const TopBar = ({ isLoggedIn, showProfile, setShowProfile, login}) => {
+const TopBar = ({ isLoggedIn, showProfile, setShowProfile, login }) => {
   const navigate = useNavigate();
   const { auth } = useAuth();
   const [loginAreaHeight, setLoginAreaHeight] = useState('0px');
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     if (showProfile) {
@@ -74,7 +75,7 @@ const TopBar = ({ isLoggedIn, showProfile, setShowProfile, login}) => {
 
   const profile = () => {
     setShowProfile(!showProfile);
-  }
+  };
 
   const Logout = async () => {
     try {
@@ -86,50 +87,76 @@ const TopBar = ({ isLoggedIn, showProfile, setShowProfile, login}) => {
       }
       localStorage.setItem('auth', false);
       setShowProfile(false);
-      navigate('/');
-      window.location.reload();
+      setTimeout(() => {
+        navigate('/');
+        window.location.reload();
+      },800);
     } catch (e) {
       console.error("An error occurred while logging out: ", e);
     }
   };
-  
-  return(
-  <div className="top-bar-content">
-    <LoginArea style={{ height: loginAreaHeight }}>
-      <div style={{ display: 'flex', flex: 1, paddingLeft: 150, paddingRight: 20 }}>
-        <ProfileImage><img src={UserImage} alt="user-image" style={{width:'100%', height:'100%'}}/></ProfileImage>
-        <h5>Welcome {auth.username}</h5>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-        <h5 style={{ marginLeft: 50 }}>Quick Links</h5>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: 50, flex: 1 }}>
-          <ProfileButton onClick={() => navigate('/profile')}><IoHomeSharp size={14} style={{ marginBottom: 5 }} /> Profile Page</ProfileButton>
-          <ProfileButton onClick={() => navigate('/question')}><FaCircleQuestion size={14} style={{ marginBottom: 5 }} /> Questions Asked</ProfileButton>
-          <ProfileButton onClick={() => navigate('/answer')}><MdQuestionAnswer size={14} style={{ marginBottom: 5 }} /> Answers</ProfileButton>
-          <ProfileButton onClick={() => Logout()}><BiLogOut size={16} style={{ marginBottom: 2 }} /> Logout</ProfileButton>
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`/search?text=${searchQuery}`);
+    }
+  };
+
+  const handleSearchClick = () => {
+    navigate(`/search?query=${searchQuery}`);
+  };
+
+  return (
+    <div className="top-bar-content">
+      <LoginArea style={{ height: loginAreaHeight }}>
+        <div style={{ display: 'flex', flex: 1, paddingLeft: 150, paddingRight: 20 }}>
+          <ProfileImage><img src={UserImage} alt="user-image" style={{ width: '100%', height: '100%' }} /></ProfileImage>
+          <h5>Welcome {auth.username}</h5>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+          <h5 style={{ marginLeft: 50 }}>Quick Links</h5>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: 50, flex: 1 }}>
+            <ProfileButton onClick={() => navigate('/profile')}><IoHomeSharp size={14} style={{ marginBottom: 5 }} /> Profile Page</ProfileButton>
+            <ProfileButton onClick={() => navigate('/question')}><FaCircleQuestion size={14} style={{ marginBottom: 5 }} /> Questions Asked</ProfileButton>
+            <ProfileButton onClick={() => navigate('/answer')}><MdQuestionAnswer size={14} style={{ marginBottom: 5 }} /> Answers</ProfileButton>
+            <ProfileButton onClick={() => Logout()}><BiLogOut size={16} style={{ marginBottom: 2 }} /> Logout</ProfileButton>
+          </div>
+        </div>
+      </LoginArea>
+      <div className="top-bar">
+        <div className="left-section">
+          <LeftSectionButton onClick={isLoggedIn ? profile : login}>
+            {isLoggedIn ? (
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <ProfileImage style={{ height: 30, width: 30 }}>
+                  <img src={UserImage} alt="user-image" style={{ width: '100%', height: '100%' }} />
+                </ProfileImage>
+                {auth.username}
+              </div>
+            ) : (
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <IoMdPerson style={{ marginRight: 5 }} /> Login
+              </div>
+            )}
+          </LeftSectionButton>
+        </div>
+        <div className="right-section" style={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <AiOutlineSearch color='white' onClick={handleSearchClick} style={{ cursor: 'pointer' }} />
+          </div>
+          <input
+            type="text"
+            placeholder="Search here ..."
+            className='search-input1'
+            style={{ backgroundColor: 'transparent', color: 'white' }}
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={handleSearch}
+          />
         </div>
       </div>
-    </LoginArea>
-    <div className="top-bar">
-      <div className="left-section">
-        <LeftSectionButton onClick={isLoggedIn ? profile : login}>
-          {isLoggedIn ? <div style={{display:'flex', alignItems:'center'}}><ProfileImage style={{height:30,width:30}}><img src={UserImage} alt="user-image" style={{width:'100%', height:'100%'}}/></ProfileImage>{auth.username}</div > : <div style={{display:'flex', alignItems:'center'}}><IoMdPerson style={{marginRight:5}}/>
-          Login</div>}
-        </LeftSectionButton>
-        {/* <LeftSectionButton onClick={()=>navigate('/askquestion')}>Add Post</LeftSectionButton> */}
-      </div>
-      <div className="right-section" style={{flex:1, display:'flex', justifyContent:'flex-end'}}>
-        <div style={{ display: 'flex', alignItems: 'center' }}><AiOutlineSearch color='white' /></div>
-        <input
-          type="text"
-          placeholder="Search here ..."
-          className='search-input1'
-          style={{ backgroundColor: 'transparent', color: 'white' }}
-        />
-      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default TopBar;
